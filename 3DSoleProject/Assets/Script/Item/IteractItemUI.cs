61171939cf2a9eb6510068b0761bea58175fa09f
한상진 <zkjx12@gmail.com>
2024-04-17 11:11:19 +0900

    ItemInteractUI
    바닥에 있는 아이템에 다가갔을 때 UI 출력 제작중

---
+++ 3DSoleProject/Assets/Script/Item/IteractItemUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IteractItemUI : MonoBehaviour
+{
+    //가까이 오면 아이템 정보를 UI를 통해서 출력
+
+    //아이템 정보
+    [SerializeField]
+    private Transform ItemBody;
+
+    //패널
+    [SerializeField]
+    private Transform cameraArm;
+
+    //private Transform itemCenter;
+
+    private void Awake()
+    {
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+       if(ItemBody != null)
+        {
+            Gizmos.color = Color.red;
+            //Gizmos.DrawSphere(ItemBody.position, halfSize);
+        }
+    }
+}
